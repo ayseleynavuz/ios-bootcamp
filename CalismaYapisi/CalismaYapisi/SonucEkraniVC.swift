0a2e9bb6feeef8994aad3d450f54c697fd95ca6b
ayseleynavuz <ayseleynavuz@gmail.com>
2024-04-16 01:06:48 +0300

    Sayfa Oluşturma - Görsel Nesne Bağlama - Sayfalar Arası Geçişler

---
+++ CalismaYapisi/CalismaYapisi/SonucEkraniVC.swift
@@ -0,0 +1,29 @@
+//
+//  SonucEkraniVC.swift
+//  CalismaYapisi
+//
+//  Created by Aleyna on 15.04.2024.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
