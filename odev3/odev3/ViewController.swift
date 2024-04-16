50fbde430af31553bda1f64837cb6c521c009673
ayseleynavuz <ayseleynavuz@gmail.com>
2024-04-16 21:39:44 +0300

    screenshot added

--- odev3/odev3/ViewController.swift
+++ odev3/odev3/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // renklendirme için
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "bgColor")
+        
+        appearance.titleTextAttributes = [.foregroundColor : UIColor(named: "textColor")!, .font: UIFont(name: "Poppins-Regular", size: 22)]
+               
+        // status bar
+        // tema siyah seçilir, telefon saati, pil durumu beyaz renkli görünür.
+        navigationController?.navigationBar.barStyle = .black
+               
+        // telefonlardaki farkdan dolayı(çentik, su damlası vb.) tasarım hatası almamak için.
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        
     }
 
 
