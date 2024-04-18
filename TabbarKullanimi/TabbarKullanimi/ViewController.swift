de904f8fcaefee5c61a5749e7d7c87fc09d04a20
ayseleynavuz <ayseleynavuz@gmail.com>
2024-04-18 21:56:14 +0300

    tabbar kullanımı

---
+++ TabbarKullanimi/TabbarKullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabbarKullanimi
+//
+//  Created by Aleyna on 18.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
