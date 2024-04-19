f4d7fa5e78d26e7f318351dbf167b6612ac7a1aa
ayseleynavuz <ayseleynavuz@gmail.com>
2024-04-19 12:07:34 +0300

    odev4 sayfa geçişleri

--- KullaniciEtkilesimi/KullaniciEtkilesimi/ViewController.swift
+++ KullaniciEtkilesimi/KullaniciEtkilesimi/ViewController.swift
@@ -14,6 +14,89 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .alert)
+        
+        
+        // iptal için
+        let iptalAction = UIAlertAction(title: "İptal", style: .cancel){ action in  // cancel mavi görünür
+            // butona tıklandığında işlem olacaksa
+            // action. -> diye kullanılır
+            print("İptal seçildi.")
+        }
+        alertController.addAction(iptalAction)
+        
+        
+        //tamam için
+        let tamamAction = UIAlertAction(title: "Tamam", style: .destructive){ action in  // destructive kırmızı görünür
+            print("Tamam seçildi.")
+        }
+        alertController.addAction(tamamAction)
+      
+        
+        
+        
+        self.present(alertController, animated: true)
+    }
+    
+    
+    
+    @IBAction func buttonActionSheet(_ sender: Any) {
+        
+        
+        let alertController = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .actionSheet)  // burada farklılık yapıldı
+        
+        let iptalAction = UIAlertAction(title: "İptal", style: .cancel){ action in  // cancel mavi görünür
+            print("İptal seçildi.")
+        }
+        alertController.addAction(iptalAction)
+        
+    
+        let tamamAction = UIAlertAction(title: "Tamam", style: .destructive){ action in  // destructive kırmızı görünür
+            print("Tamam seçildi.")
+        }
+        alertController.addAction(tamamAction)
+      
+        
+        self.present(alertController, animated: true)
+        
+    }
+    
+    
+    
+    @IBAction func buttonOzelAlert(_ sender: Any) {
+        
+        
+        let alertController = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .alert)  // burada farklılık yapıldı
+        
+        alertController.addTextField(){ textField  in  // indexi 0
+            textField.placeholder = "Veri giriniz: "
+            textField.keyboardType = .numberPad  // sadece sayılar
+            textField.isSecureTextEntry = true // şifre girerken karakteri göstermiyor
+        }
+        
+        alertController.addTextField()  // indexi 1
+        
+        let tamamAction = UIAlertAction(title: "Kaydet", style: .destructive){ action in  // destructive kırmızı görünür
+            
+            let tf = alertController.textFields![0] as UITextField
+            let tf1 = alertController.textFields![1] as UITextField
+            
+            if let alinanVeri = tf.text, let alinanVeri1 = tf1.text{
+                print("Veri : \(alinanVeri) ve \(alinanVeri1)")
+            }
+          
+        }
+        alertController.addAction(tamamAction)
+      
+        
+        self.present(alertController, animated: true)
+        
+    }
+    
+    
+    
+    
+    
 }
 
---
+++ odev4/odev4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odev4
+//
+//  Created by Aleyna on 19.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
