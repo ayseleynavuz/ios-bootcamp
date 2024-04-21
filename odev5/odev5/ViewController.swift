3f504ffa67739feca1e5063dd9a79a3ad56107d6
ayseleynavuz <ayseleynavuz@gmail.com>
2024-04-21 17:30:22 +0300

    odev5 added basic calculator

---
+++ odev5/odev5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odev5
+//
+//  Created by Aleyna on 19.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
